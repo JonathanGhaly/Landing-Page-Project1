2bb4477de13e176eb24703753534aba944336617
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2021-05-20 18:29:34 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,64 +18,64 @@
  * 
 */
 const docSections = document.getElementsByTagName('section');
-const navBar = document.createElement('ul');
-//const sectionCount = docSections.length;
+var navbar = document.createElement('ul');
+const numberOfSections = docSections.length;
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function viewPortXY(elem){
-    const pos = elem.getBoundingClientRect();
-    const x = pos.top , y=pos.bottom ;
-    return(
-        x >=0 &&
-        y <=(window.innerHeight || document.documentElement.clientHeight)
+function isInViewport(element) { //checks if an element is close to the top of the page
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
     );
 }
-function activeSection(){
-    window.addEventListener('scroll', function(){
-        
+
+function findActiveElement(){ //checks for the element closest to the top of the page
+    window.addEventListener('scroll', function() {
         for(let section of docSections){
-           if(viewPortXY(section)){
-               section.className="your-active-class";
-               break;
-           }
-           else
+            if(isInViewport(section)){
+                section.className = "your-active-class"; //sets class as active
+                break;
+            }
+            else
                 section.classList.remove('your-active-class');
         }
-        
     });
 }
 
-function scrollToSection(elemName){
-    navBar.addEventListener('click',function(event){
-        const clickedSection = document.querySelector(`[data-nav = "${event.target.textContent}"]`);
-        if(clickedSection !== null){
-            clickedSection.scrollIntoView({behavior:"smooth"});
-        }
-    });
+function scrollTO(elementName){
+    navbar.addEventListener('click', function(event){
+        const clickedItem = document.querySelector(`[data-nav = "${event.target.textContent}"]`);
+        if(clickedItem !== null)
+            clickedItem.scrollIntoView({behavior: "smooth"});
+    })
 }
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-function main1() {
+function main() {
     window.addEventListener('load', function(){ //wait for page to load
-        navBar.setAttribute('id', 'navbar__list'); //sets id
+        navbar.setAttribute('id', 'navbar__list'); //sets id
 
-        const navElement = document.querySelector('nav'); //gets the navbar element
+        const navEle = document.querySelector('nav'); //gets the navbar element
 
         for(let section of docSections){
             var listItem = document.createElement('li'); 
             listItem.className = 'menu__link';
             listItem.textContent = section.getAttribute('data-nav');
-            navBar.appendChild(listItem); //adds the created list item to the navbar
+
+            navbar.appendChild(listItem); //adds the created list item to the navbar
         }
-        navElement.appendChild(navBar); //add the navbar to the html page
+        navEle.appendChild(navbar); //add the navbar to the html page
     });
 };
+
 // build the nav
 
 
@@ -92,11 +92,8 @@ function main1() {
 */
 
 // Build menu 
-
+main();
 // Scroll to section on link click
-
+scrollTO();
 // Set sections as active
-main1();
-scrollToSection();
-activeSection();
-
+findActiveElement();
